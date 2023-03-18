8ae8864313a2c1f444ec9763e8084080cfa338f5
Anok611 <kibetanok@gmail.com>
2023-03-18 11:07:31 -0700

    6-size.c - added expected ')'

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,11 @@ long int li;
 float f;
 long long int lli;
 
-printf("Size of a char:%s", Sizeof(c);
-printf("Size of an int:%s", Sizeof(i);
-printf("Size of a long int:%s", Sizeof(li);
-printf("Size of a long long int:%s", Sizeof(lli);
-printf("Size of a float:%s", Sizeof(f);
+printf("Size of a char:%s"), Sizeof(c);
+printf("Size of an int:%s"), Sizeof(i);
+printf("Size of a long int:%s"), Sizeof(li);
+printf("Size of a long long int:%s"), Sizeof(lli);
+printf("Size of a float:%s"), Sizeof(f);
 return (0);
 }
 
