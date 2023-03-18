ff7054d495c0d1190c1ff19966eb442c0fbbade1
Anok611 <kibetanok@gmail.com>
2023-03-18 12:40:30 -0700

    6-size.c - white trailing spaces

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 /**
- * main - a program that prints the size of various types using Sizeof function 
- * Return: 0 (Success)
+ * main - a program that prints the size of various types using Sizeof function
+ * Return 0(Success)
  */
 int main(void)
 {
-char c;
-int i;
-long int li;
-float f;
-long long int lli;
+	char a;
+	int b;
+	long int c;
+	float f;
+	long long int d;
 
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a char:%lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int:%lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int:%lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int:%lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float:%lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
 
