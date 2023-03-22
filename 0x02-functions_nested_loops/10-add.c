f984a1b18feea9801fd96bfcc78bfccd7b20b52d
Anok611 <kibetanok@gmail.com>
2023-03-22 01:24:20 -0700

    10-add.c betty documentation style

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
- * add() - adds two intergers and returns result
+ * add - adds two intergers and returns result
+ * @n1: the first integer
+ * @n2: the second integer
  * Return: result
  */
 int add(int n1, int n2)
