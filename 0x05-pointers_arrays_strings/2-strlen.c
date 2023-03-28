cb3432fc49d01120a8f8230478b3101ff27f1238
Anok611 <kibetanok@gmail.com>
2023-03-28 10:17:11 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - a function that returns the length of a string
+ * @str: the string variable
+ * Return: length of a string
+ */
+int _strlen(char *str)
+{
+	size_t len = 0;
+
+	while (*str++)
+	{
+		len++;
+	}
+	return (len);
+}
